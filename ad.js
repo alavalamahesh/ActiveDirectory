5e145ad1c46f0fc0baebb5b2b6b0804a987b2204
alavalamahesh <alavala.mahesh01@gmail.com>
2019-05-15 15:44:47 +0530

    adding gitignore

--- ad.js
+++ ad.js
@@ -1,6 +1,6 @@
 const AD = require('ad');
 const ad = new AD({
-    url: "ldap://172.16.67.122",
+    url: "ldap://172.16.67.122:389",
     user: "mahesh.alavala@BRILLIOLAB.LOCAL",
     pass: "brillio@2019"
 });
@@ -16,26 +16,26 @@ const ad = new AD({
 
 (async () => {
     try {
- 
+
         var exists = await ad.user('ravi.pandey').exists();
-        console.log("User is present in the system "+exists);
+       await console.log("User is present in the system " + exists);
 
 
-        if(!exists){
+        if (!exists) {
             console.log("user not present in the AD hence creating the Record");
-           // creating new user due to not found in AD
+            // creating new user due to not found in AD
             var createduser = await ad.user().add({
                 userName: 'ravi.pandey',
                 firstName: 'ravi',
                 lastName: 'pandey',
                 location: 'India/BLR/sapphire/Users',
-                title:'Engineer',
+                title: 'Engineer',
                 //commonName:'Aishwarya p',
-                password: 'Brillio@123',
-                email:'ravi.pandey@BRILLIOLAB.LOCAL'
-                
-            }).then(
-            console.log("new user created in AD "+JSON.stringify(createduser)));
+                password: 'Brillio@123!@#$%^&&*',
+                email: 'ravi.pandey@BRILLIOLAB.LOCAL'
+
+            })
+           await console.log("new user created in AD " + JSON.stringify(createduser));
             // var details = await ad.user('ravi.pandey').get();
             // console.log("created User details "+JSON.stringify(details));
 
@@ -45,11 +45,11 @@ const ad = new AD({
             // }).catch(err => {
             //     console.log('Error getting users:', err);
             // });
-            
+
         }
 
- 
-    } catch(err) {
+
+    } catch (err) {
         console.log(err.message)
     }
 })();
